6df5e11ca829ad8911ae6c02339a677a59ff4bae
HolySianipar <holysola963@gmail.com>
2026-05-15 21:06:20 +0700

    update db

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Mengizinkan CORS secara otomatis untuk API
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
